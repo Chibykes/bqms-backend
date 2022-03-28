8dd708f32b41e293cb7a003efc3ff3abf8ca95de
Chibykes <62060851+Chibykes@users.noreply.github.com>
2022-03-28 23:42:15 +0100

    Added route for admin to see users appointments

--- api copy.js
+++
@@ -1,8 +0,0 @@
-console.log(`
-    Hello World!!! Good Morning ${your_name} 🌄😊❤️.
-    How was your night?
-
-    Inspration from code:
-    Don't be a jackass, get the fuck off your phone 
-    and do some shit else...
-`);
